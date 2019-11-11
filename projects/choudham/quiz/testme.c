55a8d50b0032325cd642bec5a2563f4f055eafdc
AC3912 <43506992+AC3912@users.noreply.github.com>
2019-11-10 20:27:44 -0500

    makefile and random tester for testme() function

--- projects/choudham/quiz/testme.c
+++ projects/choudham/quiz/testme.c
@@ -1,3 +1,10 @@
+//Amena Choudhury
+//CS362 Random Testing Quiz
+//testme.c
+//Implement random tester for
+//the function testeme()
+
+
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -6,13 +13,42 @@
 char inputChar()
 {
     // TODO: rewrite this function
-    return ' ';
+    char c; //define char variable
+    c = rand()%127; //generate random character 
+    return c; //return randomly generated character
 }
 
 char *inputString()
 {
     // TODO: rewrite this function
-    return "";
+    int r; 
+    static char s[6]; //random string stored in static array
+
+    //Determines value of each position in string based on 
+    //what the random value generated is  
+    for(int i=0; i<6; i++){
+	r=rand()%6;
+	if(r==0){ 
+    	    s[i] = 'r';
+        }
+        else if(r==1){ 
+    	    s[i] = 'e';
+    	}
+        else if(r==2){ 
+    	    s[i] = 's';
+    	}
+        else if(r==3){ 
+    	    s[i] = 'e';
+    	}
+        else if(r==4){ 
+    	    s[i] = 't';
+    	}
+        else if(r==5){ 
+    	    s[i] = '\0';
+    	}
+    }
+
+    return s;
 }
 
 void testme()
