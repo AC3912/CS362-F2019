45907960307801510f196296d4a28f3c7c509af8
AC3912 <43506992+AC3912@users.noreply.github.com>
2019-11-23 01:26:59 -0500

    Add files via upload

--- projects/choudham/dominion/randomtestcard2.c
+++ projects/choudham/dominion/randomtestcard2.c
@@ -1,4 +1,4 @@
-//randomtestcard1.c tests Baron card
+//unittest2.c tests Minion card
 
 #include "dominion.h"
 #include "dominion_helpers.h"
@@ -11,13 +11,13 @@
 
 int main()
 {
-    int seed = 500;
+    int coins;
+	int seed = 500;
     int player = 0;
-	int estateSupply;
     struct gameState G;
     int k[10] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall };
-	
-	printf("RANDOM TEST FOR BARON CARD\n\n");
+
+    printf("RANDOM TEST FOR MINION CARD\n\n");
 	
 	for(int i =0; i < 200; i++)
 	{
@@ -137,89 +137,8 @@ int main()
 		{
 			G.hand[player][4] = baron;
 			
-			srand(time(0));
-			
-			int r2 = rand()%2;
-			
-			if(r2 = 0)
-			{
-				/*G.hand[player][0] = estate;
-				
-				printf("Test 1: Check if numBuys increased by 1\n");
-				
-				//call function to play Baron card
-				caseBaron(1, 0, &G, 4, player);
-				
-				//assertion - numBuys increased by 1 = pass, otherwise fail
-				if (G.numBuys == 2)
-				{
-					printf("\tTest passed: numBuys incremented by 1\n\n");
-				}
-				else
-				{
-					printf("\tTest failed: numBuys did not increment\n\n");
-				}
-				
-				printf("Test 2: Check if player gains 4 coins\n");
-				
-				//set game state and initialize new game
-				memset(&G, 2, sizeof(struct gameState));
-				initializeGame(2, k, seed, &G);
-				
-				//Call function to play Baron card; set coin count to 0
-				G.coins = 0;
-				caseBaron(1, 1, &G, 4, player);
-
-				//assertion - if player gains 4 coins = pass, otherwise fail
-				if (G.coins == 4)
-				{
-					printf("\tTest passed: Player gained 4 coins\n\n");
-				}
-				else
-				{
-					printf("\tTest failed: Player did not gain 4 coins\n\n");
-				}*/
-				printf("Testing where error is\n\n")
-			}
-			else if
-			{
-				printf("Test 1: Check if numBuys increased by 1\n");
-				
-				//call function to play Baron card
-				caseBaron(1, 0, &G, 4, player);
-				
-				//assertion - numBuys increased by 1 = pass, otherwise fail
-				if (G.numBuys == 2)
-				{
-					printf("\tTest passed: numBuys incremented by 1\n\n");
-				}
-				else
-				{
-					printf("\tTest failed: numBuys did not increment\n\n");
-				}
-				
-				printf("Test 2: Check if player gains an Estate card\n");
-				
-				//set game state and initialize new game
-				memset(&G, 3, sizeof(struct gameState));
-				initializeGame(2, k, seed, &G);
-				
-				//estate supply count
-				estateSupply = G.supplyCount[estate];
-				
-				//call function to play Baron card
-				caseBaron(1, 0, &G, 4, player);
-
-				//assertion - if player gains Estate card = pass, otherwise fail
-				if (G.supplyCount[estate] + 1 == estateSupply)
-				{
-					printf("\tTest passed: Player gained Estate card\n\n");
-				}
-				else
-				{
-					printf("\tTest failed: Player did not gain Estate card\n\n");
-				}
-			}
+			printf("Card randomly selected: baron\n");
+			printf("No testing. Check next random selection.\n\n");
 		}				
 		else if (r==16)
 		{
