2f5807258859f9d1d68a682195535f2062dbb685
AC3912 <43506992+AC3912@users.noreply.github.com>
2019-11-23 02:16:34 -0500

    Add files via upload

--- projects/choudham/dominion/randomtestcard3.c
+++ projects/choudham/dominion/randomtestcard3.c
@@ -30,189 +30,189 @@ int main()
 		int r = rand()%27;
 		if(r==0)
 		{
-			G.hand[player][4] = curse;
+			G.hand[player1][4] = curse;
 			
 			printf("Card randomly selected: curse\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==1)
 		{
-			G.hand[player][4] = estate;
+			G.hand[player1][4] = estate;
 			
 			printf("Card randomly selected: estate\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==2)
 		{
-			G.hand[player][4] = duchy;
+			G.hand[player1][4] = duchy;
 			
 			printf("Card randomly selected: duchy\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==3)
 		{
-			G.hand[player][4] = province;
+			G.hand[player1][4] = province;
 			
 			printf("Card randomly selected: province\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==4)
 		{
-			G.hand[player][4] = copper;
+			G.hand[player1][4] = copper;
 			
 			printf("Card randomly selected: copper\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==5)
 		{
-			G.hand[player][4] = silver;
+			G.hand[player1][4] = silver;
 			
 			printf("Card randomly selected: silver\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==6)
 		{
-			G.hand[player][4] = gold;
+			G.hand[player1][4] = gold;
 			
 			printf("Card randomly selected: gold\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==7)
 		{
-			G.hand[player][4] = adventurer;
+			G.hand[player1][4] = adventurer;
 			
 			printf("Card randomly selected: adventurer\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==8)
 		{
-			G.hand[player][4] = council_room;
+			G.hand[player1][4] = council_room;
 			
 			printf("Card randomly selected: council_room\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==9)
 		{
-			G.hand[player][4] = feast;
+			G.hand[player1][4] = feast;
 			
 			printf("Card randomly selected: feast\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==10)
 		{
-			G.hand[player][4] = gardens;
+			G.hand[player1][4] = gardens;
 			
 			printf("Card randomly selected: gardens\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==11)
 		{
-			G.hand[player][4] = mine;
+			G.hand[player1][4] = mine;
 			
 			printf("Card randomly selected: mine\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==12)
 		{
-			G.hand[player][4] = remodel;
+			G.hand[player1][4] = remodel;
 			
 			printf("Card randomly selected: remodel\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==13)
 		{
-			G.hand[player][4] = smithy;
+			G.hand[player1][4] = smithy;
 			
 			printf("Card randomly selected: smithy\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==14)
 		{
-			G.hand[player][4] = village;
+			G.hand[player1][4] = village;
 			
 			printf("Card randomly selected: village\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==15)
 		{
-			G.hand[player][4] = baron;
+			G.hand[player1][4] = baron;
 			
 			printf("Card randomly selected: baron\n");
 			printf("No testing. Check next random selection.\n\n");
 		}				
 		else if (r==16)
 		{
-			G.hand[player][4] = great_hall;
+			G.hand[player1][4] = great_hall;
 			
 			printf("Card randomly selected: great_hall\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==17)
 		{
-			G.hand[player][4] = minion;
+			G.hand[player1][4] = minion;
 			
 			printf("Card randomly selected: minion\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==18)
 		{
-			G.hand[player][4] = steward;
+			G.hand[player1][4] = steward;
 			
 			printf("Card randomly selected: steward\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==19)
 		{
-			G.hand[player][4] = tribute;
+			G.hand[player1][4] = tribute;
 			
 			printf("Card randomly selected: tribute\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==20)
 		{
-			G.hand[player][4] = ambassador;
+			G.hand[player1][4] = ambassador;
 			
 			printf("Card randomly selected: ambassador\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==21)
 		{
-			G.hand[player][4] = cutpurse;
+			G.hand[player1][4] = cutpurse;
 			
 			printf("Card randomly selected: cutpurse\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==22)
 		{
-			G.hand[player][4] = embargo;
+			G.hand[player1][4] = embargo;
 			
 			printf("Card randomly selected: embargo\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==23)
 		{
-			G.hand[player][4] = outpost;
+			G.hand[player1][4] = outpost;
 			
 			printf("Card randomly selected: outpost\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==24)
 		{
-			G.hand[player][4] = salvager;
+			G.hand[player1][4] = salvager;
 			
 			printf("Card randomly selected: salvager\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==25)
 		{
-			G.hand[player][4] = sea_hag;
+			G.hand[player1][4] = sea_hag;
 			
 			printf("Card randomly selected: sea_hag\n");
 			printf("No testing. Check next random selection.\n\n");
 		}
 		else if (r==26)
 		{
-			G.hand[player][4] = treasure_map;
+			G.hand[player1][4] = treasure_map;
 			
 			printf("Card randomly selected: treasure_map\n");
 			printf("No testing. Check next random selection.\n\n");
